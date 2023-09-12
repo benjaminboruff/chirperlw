cdfc10da5d46d3b3717f351a2645f39be5591954
Benjamin H Boruff <11185240+benjaminboruff@users.noreply.github.com>
2023-09-12 12:05:17 -0400

    initial

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import "./bootstrap";
+
+// Livewire now includes these by default,
+// so they are no longer needed.
+
+// import Alpine from 'alpinejs';
+
+// window.Alpine = Alpine;
+
+// Alpine.start();
