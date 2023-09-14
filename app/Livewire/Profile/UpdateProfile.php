21e2a9977f8096887a2a0640396e915705eea22c
Benjamin H Boruff <11185240+benjaminboruff@users.noreply.github.com>
2023-09-14 08:20:38 -0400

    save button on update okay but still weird

--- app/Livewire/Profile/UpdateProfile.php
+++ app/Livewire/Profile/UpdateProfile.php
@@ -2,10 +2,31 @@
 
 namespace App\Livewire\Profile;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class UpdateProfile extends Component
 {
+    public User $user;
+    public $name;
+    public $email;
+
+    public function mount(): void
+    {
+        $this->user = User::find(Auth::user()->id);
+        $this->name = $this->user->name;
+        $this->email = $this->user->email;
+    }
+
+    public function update(): void
+    {
+        $this->user->update(
+            $this->all()
+        );
+        session()->flash('status', 'profile-updated');
+    }
+
     public function render()
     {
         return view('livewire.profile.update-profile');
